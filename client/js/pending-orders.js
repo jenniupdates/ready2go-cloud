bde2971a01de655cf2a1b3d338b6b5906da591b8
Bing <bing2609@gmail.com>
2022-11-15 22:47:29 +0800

    client: pending-order: retrieved orders from endpoint

--- client/js/pending-orders.js
+++ client/js/pending-orders.js
@@ -1,24 +1,26 @@
 const application = {
 	init: async function () {
 		const orders = await this.getOrdersAsync()
+
 		orders.forEach((order) => {
 			const {
-				orderId,
-				merchantName,
-        customerName,
-				customerAddress,
-				status,
+				trackingID,
+				merchantOrderID,
+        merchantName,
+				orderStatusDescription,
+				orderStatusDate,
+				orderStatusTime,
 			} = order
 
-      const itemElement = this.getOrderItemTemplate(`<div class="badge">${status}</div> #${orderId}`, `
-        <label class="font-bold">Destination</label>
-        <p class="mb-4">${customerAddress}</p>
-
-        <label class="font-bold">Customer</label>
-        <p class="mb-4">${customerName}</p>
-
+      const itemElement = this.getOrderItemTemplate(`<div class="badge">${orderStatusDescription}</div> #${trackingID}`, `
         <label class="font-bold">Merchant</label>
-        <p>${merchantName}</p>
+        <p class="mb-4">
+					<span class="badge badge-outline">merchant-${merchantOrderID}</span>
+					${merchantName}
+				</p>
+
+        <label class="font-bold">Order Date</label>
+        <p class="mb-4">${orderStatusDate} - ${orderStatusTime}</p>
       `)
 
       $('#order-list').append(itemElement)
@@ -43,46 +45,9 @@ const application = {
 	},
 
 	getOrdersAsync: async function () {
-		// TODO: Handle retrieval from endpoint
-
-		return [
-			{
-				orderId: "order-id-1",
-				trackingId: "tracking-id-1",
-				merchantId: "merchant-id-1",
-				merchantName: "the merchant name",
-				customerName: "the customer name",
-				customerAddress: "Blk 123, #01-01, ABC Road, 123456",
-				sellerId: "seller-id-1",
-				sellerName: "the seller name",
-				sellerAddress: "Blk 456, #01-01, DEF Road, 123456",
-				status: "pending",
-			},
-			{
-				orderId: "order-id-2",
-				trackingId: "tracking-id-2",
-				merchantId: "merchant-id-2",
-				merchantName: "the merchant name",
-				customerName: "the customer name",
-				customerAddress: "Blk 123, #01-01, ABC Road, 123456",
-				sellerId: "seller-id-2",
-				sellerName: "the seller name",
-				sellerAddress: "Blk 456, #01-01, DEF Road, 123456",
-				status: "pending",
-			},
-			{
-				orderId: "order-id-3",
-				trackingId: "tracking-id-3",
-				merchantId: "merchant-id-3",
-				merchantName: "the merchant name",
-				customerName: "the customer name",
-				customerAddress: "Blk 123, #01-01, ABC Road, 123456",
-				sellerId: "seller-id-3",
-				sellerName: "the seller name",
-				sellerAddress: "Blk 456, #01-01, DEF Road, 123456",
-				status: "pending",
-			},
-		]
+		const response = await fetch("https://n2t1ei2vci.execute-api.ap-southeast-1.amazonaws.com/prod")
+		const result = (await response.text()).replace(/'/g, '"')
+		return JSON.parse(result)
 	},
 }
 
